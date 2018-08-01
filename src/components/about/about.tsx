7072f20ea7563d874ea8a67852a33d8495f79ade
Imamudin Naseem <naseem1992@gmail.com>
2018-08-01 13:01:09 +0530

    adding about me text

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -2,13 +2,16 @@
  * Created by imamudinnaseem on 7/22/18
  */
 import {Component, h} from "preact";
+import Card from "../card/card";
 
 export default class About extends Component {
     render() {
         return (
             <section id="about">
                 <header>About</header>
-                <div></div>
+                <div className="about-me">I am a Software Engineer, working on web for past 4 years to make it a better
+                    place
+                </div>
             </section>
         )
     }
