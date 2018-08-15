d93dfe18a2e0abe3bf6c4b3709f88e969d0af47a
Imamudin Naseem <naseem1992@gmail.com>
2018-08-15 22:28:42 +0530

    excluding author

--- config/production.js
+++ config/production.js
@@ -23,7 +23,7 @@ module.exports = {
         },
         plugins: [
             new WebpackCleanupPlugin({
-                exclude: ["CNAME", "favicon.ico", "images/author_avatar.jpg"]
+                exclude: ["CNAME", "favicon.ico", "images/author.jpg"]
             }),
             new MiniCssExtractPlugin({
                 filename: '[name]' + hash + '.css',
